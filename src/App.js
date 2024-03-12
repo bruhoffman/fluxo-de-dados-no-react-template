5560e3a3b588b3e1de8e8b76691b98ff7fdde101
BrunoHoffman <bruhoffman@gmail.com>
2024-03-11 22:10:09 -0300

    Exercícios de sala de aula

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled, { createGlobalStyle } from "styled-components";
 import FormularioPostagem from "./components/FormularioPostagem/FormularioPostagem";
 import TelaDaPostagem from "./components/TelaDaPostagem/TelaDaPostagem";
+import { useState } from "react";
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -17,15 +18,55 @@ const Container = styled.div`
 `;
 
 function App() {
+
+  const [titulo, setTitulo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [dadosForm, setDadosForm] = useState({})
+
+  const onChangeTitulo = (event) => {
+    setTitulo(event.target.value);
+  }
+
+  const onChangeImagem = (e) => {
+    setImagem(e.target.value);
+  }
+
+  const onChangeDescricao = (e) => {
+    setDescricao(e.target.value);
+  }
+
+  const atualizar = (e) => {
+    e.preventDefault()
+
+    const newForm = {
+      titulo,
+      descricao,
+      imagem
+    }
+    setDadosForm(newForm)
+    setTitulo("")
+    setDescricao("")
+    setImagem("")
+  }
+
   return (
     <>
       <GlobalStyle />
 
       <Container>
 
-        <FormularioPostagem />
+        <FormularioPostagem
+          titulo={titulo}
+          onChangeTitulo={onChangeTitulo}
+          imagem={imagem}
+          onChangeImagem={onChangeImagem}
+          descricao={descricao}
+          onChangeDescricao={onChangeDescricao}
+          atualizar={atualizar}
+        />
 
-        <TelaDaPostagem />
+        <TelaDaPostagem dadosForm={dadosForm} />
 
       </Container>
     </>
